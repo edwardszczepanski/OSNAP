395d70292320b35170a6baec8167467a5f3faacb
edwardszczepanski <edward@cs.uoregon.edu>
2017-01-13 00:42:04 +0000

    progress

--- install_daemons.sh
+++ install_daemons.sh
@@ -1,16 +1,20 @@
 #/bin/bash
-alias arg1=$1
 
 git clone https://github.com/postgres/postgres.git
-cd postgres/config
-make
-cd ../..
-curl -O http://mirror.metrocast.net/apache//httpd/httpd-2.4.25.tar.gz
-gzip -d httpd-2.4.25.tar.gz
-tar xvf httpd-2.4.25.tar.gz
-cd httpd-2.4.25.tar.gz
-./configure --prefix=arg1
+cd postgres
+./configure --prefix=$1
 make
 make install
-arg1/bin/apachectl -k start # testing
+cd ..
+curl -O http://mirror.metrocast.net/apache//httpd/httpd-2.4.25.tar.gz
+gzip -d httpd-2.4.25.tar.gz
+tar xvf httpd-2.4.25.tar
+cd httpd-2.4.25
+./configure --prefix=$1
+make
+make install
+"arg1/bin/apachectl -k start # testing
+sed -i '/Listen 80/c\Listen 8080' $1/conf/httpd.conf
+cd ..
 
+export PATH=$1/bin:$PATH
