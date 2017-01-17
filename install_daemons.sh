3fe2145bff5205c3b293462602f3fb8da755e829
edwardszczepanski <edward@cs.uoregon.edu>
2017-01-17 08:10:56 +0000

    hmm

--- install_daemons.sh
+++ install_daemons.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 export PATH=$1/bin:$PATH
 
-git clone https://github.com/postgres/postgres.git $1
+git clone -b REL9_5_STABLE https://github.com/postgres/postgres.git $1
 cd $1
 ./configure --prefix=$1
 make
@@ -10,8 +10,10 @@ make install
 curl -O http://mirror.metrocast.net/apache//httpd/httpd-2.4.25.tar.gz
 gzip -d httpd-2.4.25.tar.gz
 tar -xvf httpd-2.4.25.tar
+rm httpd-2.4.25.tar httpd-2.4.25.tar.gz
 
 cd $1/httpd-2.4.25
-./configure --prefix=$1 --with-port-8080
+./configure --prefix=$1
+sed -i '/Listen 80/c\Listen 8080' $1/conf/httpd.conf
 make
 make install
