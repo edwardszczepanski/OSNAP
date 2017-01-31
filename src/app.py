e8f6aa5ac61a0b9d0b319be5bfe2d1a527f109db
edward <edward@cs.uoregon.edu>
2017-01-31 04:04:03 +0000

    assignment 3 work

---
+++ src/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, session, g, redirect, url_for, abort, flash
+
+app = Flask(__name__)
+
+@app.route('/report_filter')
+def report_filter():
+    return render_template('report_filter.html')
+
+@app.route('/facility_inventory')
+def facility_inventory():
+    return render_template('facility_inventory.html')
+
+@app.route('/in_transit')
+def in_transit():
+    return render_template('in_transit.html')
+
+@app.route('/logout')
+def logout():
+    return render_template('logout.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'USERNAME':#!= app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != 'PASSWORD':#app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            session['username'] = request.form['username']
+            session['password'] = request.form['password']
+            flash('You were logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    app.debug = True
+
+    app.run(host='0.0.0.0', port=8080)
+    #app.run(port=8080)
