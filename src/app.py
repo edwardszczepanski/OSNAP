b766867a795736faa4b16604e2740aec84cd7b10
edward <edward@cs.uoregon.edu>
2017-02-05 20:49:40 +0000

    progress

--- src/app.py
+++ src/app.py
@@ -5,32 +5,40 @@ app = Flask(__name__)
 
 @app.route('/report_filter')
 def report_filter():
+    if request.method == 'POST':
+        return redirect(url_for('in_transit'))
     return render_template('report_filter.html')
+
+@app.route('/facility_inventory')
+def facility_inventory():
+    return render_template('facility_inventory.html')
+
+@app.route('/in_transit')
+def in_transit():
+    return render_template('in_transit.html')
 
 @app.route('/logout')
 def logout():
     session['logged_in'] = False
-    return render_template('logout.html')
+    return render_template('login.html')
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'USERNAME':#!= app.config['USERNAME']:
+        if False and request.form['username'] != 'USERNAME':#!= app.config['USERNAME']:
             error = 'Invalid username'
-        elif request.form['password'] != 'PASSWORD':#app.config['PASSWORD']:
+        elif False and request.form['password'] != 'PASSWORD':#app.config['PASSWORD']:
             error = 'Invalid password'
         else:
             session['logged_in'] = True
             session['username'] = request.form['username']
             session['password'] = request.form['password']
-            flash('You were logged in')
+            flash('Welcome ' + session['username'] + '!')
             return redirect(url_for('report_filter'))
     return render_template('login.html', error=error)
 
 if __name__ == '__main__':
     app.secret_key = 'super secret key'
     app.debug = True
-
     app.run(host='0.0.0.0', port=8080)
-    #app.run(port=8080)
