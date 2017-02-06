c857eeda2249210d997c68f0e64508dc7a25fb58
edward <edward@cs.uoregon.edu>
2017-02-06 06:34:02 +0000

    Working statements

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session, g, redirect, url_for, abort, flash
 import sys, psycopg2
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -9,6 +10,7 @@ def report_filter():
     if request.method == 'POST':
         if 'facility_name' in request.form:
             session['facility_name'] = request.form['facility_name']
+            session['facility_date'] = request.form['facility_date']
             return redirect(url_for('facility_inventory'))
         elif 'in_transit' in request.form:
             session['in_transit'] = request.form['in_transit']
@@ -17,30 +19,68 @@ def report_filter():
 
 @app.route('/facility_inventory')
 def facility_inventory():
+    sql_query = "SELECT * FROM assets"
+    facility_defined = False
+    if session['facility_name'] != "*":
+        sql_query += " WHERE fcode='" + session['facility_name'] + "'"
+        facility_defined = True
+
+
+    if not session['facility_date'] == "":
+        dt = str(datetime.strptime(session['facility_date'], "%Y-%m-%d"))
+        print(dt)
+        if facility_defined:
+            sql_query += " AND "
+        else:
+            sql_query += " WHERE "
+        sql_query += "('" + dt + "') >= arrive_date"
+        sql_query += " AND ('" + dt + "') <= depart_date"
+
+    sql_query += ";"
+    print(sql_query)
+
     connect()
-    cursor.execute("SELECT * FROM assets;")
+    cursor.execute(sql_query)
+    res = cursor.fetchall()
+    processed_data = []
+    for r in res:
+        item = {}
+        item['fcode'] = r[0]
+        item['asset_tag'] = r[1]
+        item['src_facility'] = r[2]
+        item['dest_facility'] = r[3]
+        item['arrive_date'] = r[4]
+        item['depart_date'] = r[5]
+        item['expunge_date'] = r[6]
+        processed_data.append(item)
+    session['processed_data_session_name'] = processed_data
+
+    return render_template('facility_inventory.html')
+
+@app.route('/in_transit')
+def in_transit():
+    connect()
+    cursor.execute("SELECT * FROM convoys;")
     res = cursor.fetchall()
     print(res)
     processed_data = []
     for r in res:
         item = {}
-        item['asset_pk'] = r[0]
-        item['asset_tag'] = r[2]
+        item['convoy_pk'] = r[0]
+        item['source_fk'] = r[2]
+        item['dest_fk'] = r[3]
+        item['depart_dt'] = r[4]
+        item['arrive_dt'] = r[5]
         processed_data.append(item)
     session['processed_data_session_name'] = processed_data
     print(processed_data)
 
-    #return session['facility_name']
-    return render_template('facility_inventory.html')
-
-@app.route('/in_transit')
-def in_transit():
     return render_template('in_transit.html')
 
 @app.route('/logout')
 def logout():
     session['logged_in'] = False
-    return render_template('login.html')
+    return render_template('logout.html')
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
