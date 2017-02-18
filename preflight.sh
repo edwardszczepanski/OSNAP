2dcbe5a517491bdb0c3c99806d43d0a47850b40e
edward <edward@cs.uoregon.edu>
2017-02-18 23:34:19 +0000

    More stuff working

--- preflight.sh
+++ preflight.sh
@@ -1,12 +1,13 @@
 #! /usr/bin/bash
 
-if [ "$#" -ne 1]; then
+if [ "$#" -ne 1 ]; then
     echo "Usage: ./preflight.sh <dbname>"
     exit;
 fi
 
 cd sql
 psql $1 -f create_tables.sql
+cd ..
 
 cp -R src/* $HOME/wsgi
 apachectl restart
--- src/app.py
+++ src/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request, session, g, redirect, url_for, abort, flash
 import sys, psycopg2, json
 from datetime import datetime
-from config import dbname, dbhost, dbport, lost_priv, lost_pub, user_pub, prod_pub
+from config import dbname, dbhost, dbport
 
 app = Flask(__name__)
+app.secret_key = 'super secret key'
 
 @app.route('/report_filter', methods=['GET', 'POST'])
 def report_filter():
+    return render_template('report_filter.html')
     val = None
     if request.method == 'POST':
         if 'facility_name' in request.form:
@@ -18,27 +20,6 @@ def report_filter():
             session['facility_inventory'] = False;
         return redirect(url_for('reports'))
     return render_template('report_filter.html')
-
-@app.route('/reports')
-def reports():
-    sql_query = sql_query_generator()
-    connect()
-    cursor.execute(sql_query)
-    res = cursor.fetchall()
-    processed_data = []
-    for r in res:
-        item = {}
-        item['fcode'] = r[0]
-        item['asset_tag'] = r[1]
-        item['src_facility'] = r[2]
-        item['dest_facility'] = r[3]
-        item['arrive_date'] = r[4]
-        item['depart_date'] = r[5]
-        item['expunge_date'] = r[6]
-        processed_data.append(item)
-    session['processed_data_session_name'] = processed_data
-
-    return render_template('reports.html')
 
 @app.route('/logout')
 def logout():
@@ -64,8 +45,6 @@ def login():
 def connect():
     global cursor
     global conn
-    #connection_string = "host='localhost' port='" + str(dbport) + "' dbname='" + db_name + "' user='osnapdev' password='secret'"
-    #conn = psycopg2.connect(connection_string)
     conn = psycopg2.connect(dbname=dbname, host=dbhost, port=dbport)
     cursor = conn.cursor()
     work_mem = 2048
@@ -73,11 +52,6 @@ def connect():
     print("Connected to server")
 
 if __name__ == '__main__':
-    #global db_name
-    #if len(sys.argv) > 1:
-    #    db_name = sys.argv[1]
-    #else:
-    #    db_name = dbname
-    #app.secret_key = 'super secret key'
-    #app.debug = True
-    #app.run(host='0.0.0.0', port=8080)
+    connect()
+    app.debug = True
+    app.run()
