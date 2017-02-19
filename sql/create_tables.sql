a833b5f14dafeba7c008abd401f08a174c8eccf5
edward <edward@cs.uoregon.edu>
2017-02-19 03:23:27 +0000

    Them docs

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,7 +1,8 @@
 
 /*
- * user_pk is a serial primary key for more flexibility.
- * username and password have been limited to 16 characters as per the project description
+ * user_pk is a serial primary key for more flexibility rather than using the username value.
+ * username and password have been limited to 16 characters as per the project description.
+ * Only one table was created as I don't see a big reason to have two separate tables.
  */
 CREATE TABLE users(
     user_pk serial primary key, 
