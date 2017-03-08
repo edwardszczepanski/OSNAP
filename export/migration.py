be303f6bdc71be64fce1186a116735a61ff49278
edward <edward@cs.uoregon.edu>
2017-03-08 02:18:34 +0000

    Finished export functionality

--- export/migration.py
+++ export/migration.py
@@ -54,15 +54,31 @@ def assets():
             mywriter.writerow([asset_tag] + [description] + [facility] + [acquired] + [disposed])
 
 def transfers():
-    with open('facilities.csv', 'w', newline='\n') as csvfile:
+    with open('transfers.csv', 'w', newline='\n') as csvfile:
         mywriter = csv.writer(csvfile, quotechar="'", quoting=csv.QUOTE_MINIMAL)
-        mywriter.writerow(['fcode', 'common_name'])
-        cursor.execute("SELECT * FROM facilities;")
+        mywriter.writerow(['asset_tag', 'request_by', 'request_dt', 'approve_by', 'approve_dt', 'source', 'destination', 'load_dt', 'unload_dt'])
+        cursor.execute("SELECT * FROM requests;")
         response = cursor.fetchall()
         for entry in response:
-            fcode = entry[1]
-            common_name = entry[2]
-            mywriter.writerow([fcode] + [common_name])
+            cursor.execute("SELECT * FROM assets WHERE asset_pk=" + str(entry[1]) + ";")
+            asset_tag = cursor.fetchone()[1]
+            cursor.execute("SELECT * FROM users WHERE user_pk=" + str(entry[2]) + ";")
+            request_by = cursor.fetchone()[2]
+            request_dt = str(entry[5].strftime('%Y-%m-%d %H:%M:%S'))
+            cursor.execute("SELECT * FROM facilities WHERE facility_pk=" + str(entry[3]) + ";")
+            source = cursor.fetchone()[2]
+            cursor.execute("SELECT * FROM facilities WHERE facility_pk=" + str(entry[4]) + ";")
+            # Below are the approved ones
+            destination = cursor.fetchone()[2]
+            approve_dt = str(entry[6].strftime('%Y-%m-%d %H:%M:%S'))
+            cursor.execute("SELECT * FROM users WHERE user_pk=" + str(entry[8]) + ";")
+            approve_by = cursor.fetchone()[2]
+            # These two queries are passed back and may not be complete
+            cursor.execute("SELECT * FROM in_transit WHERE request_fk=" + str(entry[0]) + ";")
+            load_dt = str(cursor.fetchone()[2].strftime('%Y-%m-%d %H:%M:%S'))
+            cursor.execute("SELECT * FROM in_transit WHERE request_fk=" + str(entry[0]) + ";")
+            unload_dt = str(cursor.fetchone()[3].strftime('%Y-%m-%d %H:%M:%S'))
+            mywriter.writerow([asset_tag] + [request_by] + [request_dt] + [approve_by] + [approve_dt] + [source] + [destination] + [load_dt] + [unload_dt])
 
 def connect():
     global cursor
